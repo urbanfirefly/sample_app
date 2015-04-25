484866cb46586dcd5c43a0394c8c5af5e9b759ab
Dmitro <dmitrogolovan@gmail.com>
2015-04-25 12:06:24 +0300

    "Contact Pge added"

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 require 'rails_helper'
 describe "Static pages" do
 
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -10,7 +12,7 @@ describe "Static pages" do
     end
     it "should have title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Home')
+      expect(page).to have_title("#{base_title} | Home")
     end
   end
 
@@ -21,9 +23,21 @@ describe "Static pages" do
     end
     it "should have title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Help')
+      expect(page).to have_title("#{base_title} |  Help")
     end
   end
+
+  describe "Contact page" do
+    it "should have content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+    it "should have title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
   describe "About page" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
@@ -31,7 +45,7 @@ describe "Static pages" do
     end
     it "should have title 'About'" do
       visit '/static_pages/about'
-      expect(page).to have_title('About')
+      expect(page).to have_title("#{base_title} | About")
     end
   end
 end
